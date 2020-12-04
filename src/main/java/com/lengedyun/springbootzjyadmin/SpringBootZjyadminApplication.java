9cb0242be3032712e729a0b9e581b8da94217db0
zhangjianyun <zhangjianyun_kd@163.com>
2020-12-04 22:10:32 +0800

    Initial commit

---
+++ src/main/java/com/lengedyun/springbootzjyadmin/SpringBootZjyadminApplication.java
@@ -0,0 +1,15 @@
+package com.lengedyun.springbootzjyadmin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class SpringBootZjyadminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootZjyadminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lengedyun/springbootzjyadmin/SpringBootZjyadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lengedyun.springbootzjyadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootZjyadminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
